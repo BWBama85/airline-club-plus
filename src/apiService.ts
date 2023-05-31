2dfad4af3cc8268116d422e387bac0438f664853
Brent Wilson <brent@thewilsonnet.com>
2023-05-30 20:55:04 -0500

    formatting

--- src/apiService.ts
+++ src/apiService.ts
@@ -1,5 +1,5 @@
 export async function fetchData(url: string): Promise<any> {
-  const response: Response = await fetch(url);
-  const fetchedData: any = await response.json();
-  return fetchedData;
+  const response: Response = await fetch(url)
+  const fetchedData: any = await response.json()
+  return fetchedData
 }
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,24 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState("")
 
   return (
     <div
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 16,
-      }}
-    >
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
       <h1>Airline Club Plus</h1>
       <p>Improves the Airline-Club.com game experience.</p>
       <p>
         Source: <a href="https://github.com/brentwilson/airline-club-plus">Airline Club Plus</a>
       </p>
-      <footer>Created by Brent Wilson</footer>{' '}
+      <footer>Created by Brent Wilson</footer>{" "}
     </div>
-  );
+  )
 }
 
-export default IndexPopup;
+export default IndexPopup
