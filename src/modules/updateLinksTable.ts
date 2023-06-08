7ceb8fe4d075d3f327fdae9c2e4a104f4739ca26
Brent Wilson <brent@thewilsonnet.com>
2023-06-08 06:24:29 -0500

    Refactoring

--- src/modules/updateLinksTable.ts
+++ src/modules/updateLinksTable.ts
@@ -1,168 +1,171 @@
-import { appendCell, prependCell } from "~/helpers/tables"
-import { selectLinkFromTable } from "~/helpers/tables"
-import { getShortModelName } from "~/helpers/tables"
+import { appendCell, getShortModelName, prependCell, selectLinkFromTable } from "~/helpers/tables"
 import { getStyleFromTier, getTierFromPercent } from "~/helpers/utils"
 
 export function updateLinksTable(sortProperty: string, sortOrder: string) {
-  let colorKeyMaps: { [key: string]: WeakMap<Link, any> } = {}
+  const colorKeyMaps: Record<string, WeakMap<Link, any>> = {}
   const linksTable = window.document.querySelector("#linksCanvas #linksTable")
-  while (linksTable?.firstChild) {
-    linksTable.removeChild(linksTable.firstChild)
+
+  if (linksTable) {
+    linksTable.innerHTML = ""
   }
 
-  window.loadedLinks = window.sortPreserveOrder(window.loadedLinks, sortProperty, sortOrder == "ascending")
+  const orderedLinks = window.sortPreserveOrder(window.loadedLinks, sortProperty, sortOrder === "ascending")
+  window.loadedLinks = orderedLinks
 
-  function getKeyedStyleFromLink(link: Link, keyName: string, ...args: number[]): any {
-    if (!colorKeyMaps[keyName]) {
-      colorKeyMaps[keyName] = new WeakMap<Link, any>()
-    } else if (colorKeyMaps[keyName].has(link)) {
-      return colorKeyMaps[keyName].get(link)
-    }
+  window.loadedLinks.forEach((link: Link) => {
+    const row = createLinkRow(colorKeyMaps, link)
 
-    const data = window.loadedLinks.map((l) => l[keyName])
-
-    const avg = data.reduce((sum, acc) => (sum += acc), 0) / window.loadedLinks.length
-    const max = Math.max(...data)
-    const min = Math.max(Math.min(...data), 0)
-
-    const tier = getTierFromPercent(link[keyName], args[0] !== undefined ? args[0] : min, args[1] || avg * 0.618)
-    if (!link.tiers) {
-      link.tiers = {}
-    }
-
-    link.tiers[keyName] = tier
-
-    const stylesFromGoodToBad = [
-      "color:#29FF66;",
-      "color:#5AB874;",
-      "color:inherit;",
-      "color:#FA8282;",
-      "color:#FF6969;",
-      "color:#FF3D3D;font-weight: bold;"
-    ]
-
-    const colorResult = getStyleFromTier(tier)
-
-    colorKeyMaps[keyName].set(link, colorResult)
-
-    return colorResult
-  }
-
-  window.loadedLinks.forEach((link: Link, index: number) => {
-    const row = window.document.createElement("div")
-    row.className = "table-row clickable"
-    row.onclick = () => selectLinkFromTable(row, link.id)
-
-    const srcAirportFull = window.getAirportText(link.fromAirportCity, link.fromAirportCode)
-    const destAirportFull = window.getAirportText(link.toAirportCity, link.toAirportCode)
-
-    appendCell(
-      row,
-      srcAirportFull,
-
-      srcAirportFull.slice(-4, -1)
-    )
-    appendCell(
-      row,
-      destAirportFull,
-
-      destAirportFull.slice(-4, -1)
-    )
-    appendCell(row, "", getShortModelName(link.model), true)
-    appendCell(row, "", link.distance + "km", false, "right")
-    appendCell(row, "", link.totalCapacity + " (" + link.frequency + ")", false, "right")
-    appendCell(row, "", link.totalPassengers.toString(), false, "right")
-
-    const lfBreakdown = {
-      economy: link.passengers.economy / link.capacity.economy,
-      business: link.passengers.business / link.capacity.business,
-      first: link.passengers.first / link.capacity.first
-    }
-
-    let lfBreakdownText =
-      link.totalLoadFactor === 100
-        ? "100"
-        : [lfBreakdown.economy, lfBreakdown.business, lfBreakdown.first]
-            .map((v) => (v ? Math.floor(100 * v) : "-"))
-            .join("/")
-
-    appendCell(row, "", lfBreakdownText + "%", false, "right", getKeyedStyleFromLink(link, "totalLoadFactor", 0, 100))
-    appendCell(
-      row,
-      "",
-      Math.round(link.satisfaction * 100) + "%",
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "satisfaction", 0, 1)
-    )
-    appendCell(
-      row,
-      "$" + new Intl.NumberFormat("en-US").format(link.revenue),
-      "$" + new Intl.NumberFormat("en-US").format(link.revenue),
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "revenue")
-    )
-    appendCell(
-      row,
-      "$" + new Intl.NumberFormat("en-US").format(link.profit),
-      "$" + new Intl.NumberFormat("en-US").format(link.profit),
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "profit")
-    )
-    appendCell(
-      row,
-      "",
-      (link.profitMargin > 0 ? "+" : "") + Math.round(link.profitMargin) + "%",
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "profitMarginPercent", 0, 136.5)
-    )
-    appendCell(
-      row,
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerPax),
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerPax),
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "profitPerPax")
-    )
-    appendCell(
-      row,
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerFlight),
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerFlight),
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "profitPerFlight")
-    )
-    appendCell(
-      row,
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerHour),
-      "$" + new Intl.NumberFormat("en-US").format(link.profitPerHour),
-      false,
-      "right",
-      getKeyedStyleFromLink(link, "profitPerHour")
-    )
-
-    if (window.selectedLink === link.id) {
-      row.classList.add("selected")
-    }
-
-    const tiersRank = (link.tiersRank = Object.keys(link.tiers || {}).reduce(
-      (sum, key) => sum + link.tiers[key] + (key === "profit" && link.tiers[key] === 0 ? -1 : 0),
-      0
-    ))
-
-    prependCell(row, "", link.tiersRank.toString())
-
-    if (tiersRank < 2) {
-      row.style.textShadow = "0 0 3px gold"
-    }
-
-    if (tiersRank > 27) {
-      row.style.textShadow = "0 0 3px red"
-    }
-
-    linksTable.appendChild(row)
+    linksTable?.appendChild(row)
   })
+}
+
+function getKeyedStyleFromLink(
+  colorKeyMaps: Record<string, WeakMap<Link, any>>,
+  link: Link,
+  keyName: string,
+  ...args: number[]
+): any {
+  if (!colorKeyMaps[keyName]) {
+    colorKeyMaps[keyName] = new WeakMap<Link, any>()
+  } else if (colorKeyMaps[keyName].has(link)) {
+    return colorKeyMaps[keyName].get(link)
+  }
+
+  const data = window.loadedLinks.map((l) => l[keyName])
+  const avg = data.reduce((sum, acc) => (sum += acc), 0) / window.loadedLinks.length
+  const min = Math.max(0, ...data)
+
+  const tier = getTierFromPercent(link[keyName], args[0] !== undefined ? args[0] : min, args[1] || avg * 0.618)
+  link.tiers = link.tiers || {}
+  link.tiers[keyName] = tier
+
+  const colorResult = getStyleFromTier(tier)
+  colorKeyMaps[keyName].set(link, colorResult)
+
+  return colorResult
+}
+
+function createLinkRow(colorKeyMaps: Record<string, WeakMap<Link, any>>, link: Link) {
+  const row = window.document.createElement("div")
+  row.className = "table-row clickable"
+  row.onclick = () => selectLinkFromTable(row, link.id)
+
+  populateLinkRowCells(colorKeyMaps, row, link)
+
+  return row
+}
+
+function populateLinkRowCells(colorKeyMaps: Record<string, WeakMap<Link, any>>, row: HTMLDivElement, link: Link) {
+  const srcAirportFull = window.getAirportText(link.fromAirportCity, link.fromAirportCode)
+  const destAirportFull = window.getAirportText(link.toAirportCity, link.toAirportCode)
+  const lfBreakdown = {
+    economy: link.passengers.economy / link.capacity.economy,
+    business: link.passengers.business / link.capacity.business,
+    first: link.passengers.first / link.capacity.first
+  }
+
+  const lfBreakdownText = generateLfBreakdownText(link, lfBreakdown)
+
+  appendCell(row, srcAirportFull, srcAirportFull.slice(-4, -1))
+  appendCell(row, destAirportFull, destAirportFull.slice(-4, -1))
+  appendCell(row, "", getShortModelName(link.model), true)
+  appendCell(row, "", link.distance + "km", false, "right")
+  appendCell(row, "", link.totalCapacity + " (" + link.frequency + ")", false, "right")
+  appendCell(row, "", link.totalPassengers.toString(), false, "right")
+  appendCell(
+    row,
+    "",
+    lfBreakdownText + "%",
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "totalLoadFactor", 0, 100)
+  )
+  appendCell(
+    row,
+    "",
+    Math.round(link.satisfaction * 100) + "%",
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "satisfaction", 0, 1)
+  )
+  appendCell(
+    row,
+    formatCurrency(link.revenue),
+    formatCurrency(link.revenue),
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "revenue")
+  )
+  appendCell(
+    row,
+    formatCurrency(link.profit),
+    formatCurrency(link.profit),
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "profit")
+  )
+  appendCell(
+    row,
+    "",
+    (link.profitMargin > 0 ? "+" : "") + Math.round(link.profitMargin) + "%",
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "profitMarginPercent", 0, 136.5)
+  )
+  appendCell(
+    row,
+    formatCurrency(link.profitPerPax),
+    formatCurrency(link.profitPerPax),
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "profitPerPax")
+  )
+  appendCell(
+    row,
+    formatCurrency(link.profitPerFlight),
+    formatCurrency(link.profitPerFlight),
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "profitPerFlight")
+  )
+  appendCell(
+    row,
+    formatCurrency(link.profitPerHour),
+    formatCurrency(link.profitPerHour),
+    false,
+    "right",
+    getKeyedStyleFromLink(colorKeyMaps, link, "profitPerHour")
+  )
+
+  link.tiersRank = Object.keys(link.tiers || {}).reduce(
+    (sum, key) => sum + link.tiers[key] + (key === "profit" && link.tiers[key] === 0 ? -1 : 0),
+    0
+  )
+
+  if (link.tiersRank < 2) {
+    row.style.textShadow = "0 0 3px gold"
+  }
+
+  if (link.tiersRank > 27) {
+    row.style.textShadow = "0 0 3px red"
+  }
+
+  prependCell(row, link.tiersRank.toString(), link.tiersRank.toString())
+  if (window.selectedLink === link.id) {
+    row.classList.add("selected")
+  }
+}
+
+function formatCurrency(num: number): string {
+  return "$" + new Intl.NumberFormat("en-US").format(num)
+}
+
+function generateLfBreakdownText(link: Link, lfBreakdown: Record<string, number>): string {
+  if (link.totalLoadFactor === 100) {
+    return "100"
+  }
+
+  return Object.values(lfBreakdown)
+    .map((v) => (v ? Math.floor(100 * v) : "-"))
+    .join("/")
 }
