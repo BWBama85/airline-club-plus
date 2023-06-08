21bc859bb79b55210e0bf46c3b86889c2ecbe80b
Brent Wilson <brent@thewilsonnet.com>
2023-06-08 01:42:29 -0500

    Remove unused types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,88 +1,53 @@
 interface Window {
   $: JQueryStatic
-  updateCustomLinkTableHeader: () => void
-  refreshLinkDetails: (LinkId: number) => Promise<void>
-  updateLoadedLinks: (links: Link[]) => void
-  loadLinksTable: () => Promise<void>
   getAirportText: (city: string, code: string) => string
+  loadLinksTable: () => Promise<void>
+  refreshLinkDetails: (LinkId: number) => Promise<void>
   sortPreserveOrder: (links: Link[], sortProperty: string, ascending: boolean) => Link[]
-  getLoadFactorsFor: (consumption: Link) => number[]
   toggleLinksTableSortOrder: (sortHeader: HTMLElement) => void
-  selectedLink: number
+  updateLoadedLinks: (links: Link[]) => void
+  activeAirline?: ActiveAirline
   link: Link
   loadedLinks: Link[]
-  activeAirline?: ActiveAirline
+  selectedLink: number
 }
 
 interface Link {
   id?: number
-  fromAirportId?: number
-  fromAirportCity?: string
-  fromAirportCode?: string
-  fromCountryCode?: string
-  toAirportId?: number
-  toAirportCity?: string
-  toAirportCode?: string
-  toCountryCode?: string
-  flightCode?: string
-  flightType?: string
-  computedQuality?: number
-  future?: any
-  price?: number
-  distance?: number
-  satisfaction?: number
-  frequency?: number
-  duration?: number
-  model?: string
+  assignedAirplanes?: AssignedAirplane[]
   capacity?: Capacity
   capacityHistory?: Capacity
+  duration?: number
+  frequency?: number
+  model?: string
   passengers?: Capacity
-  soldSeats?: Capacity
-  totalCapacity?: number
-  totalCapacityHistory?: number
-  totalPassengers?: number
-  totalLoadFactor?: number
-  assignedAirplanes?: AssignedAirplane[]
-  revenue?: number
   profit?: number
   profitMarginPercent?: number
   profitMargin?: number
   profitPerPax?: number
   profitPerFlight?: number
   profitPerHour?: number
-  tiers?: { [key: string]: any }
-  tiersRank?: number
-}
-
-interface Capacity {
-  economy: number
-  business: number
-  first: number
-}
-
-interface AssignedAirplane {
-  airplane: Airplane
-}
-
-interface Airplane {
-  name: string
+  revenue?: number
+  totalCapacity?: number
+  totalCapacityHistory?: number
+  totalPassengers?: number
+  totalLoadFactor?: number
 }
 
 interface ActiveAirline {
   id: number
 }
 
-interface DataSourceEntry {
-  [key: string]: any
+interface Airplane {
+  name: string
 }
 
-interface averageFromSubKey {
-  [key: string]: any
+interface AssignedAirplane {
+  airplane: Airplane
 }
 
-type EntryType = {
-  label: string
-  value: any
-  color?: string
-  issliced?: string
+interface Capacity {
+  economy: number
+  business: number
+  first: number
 }
