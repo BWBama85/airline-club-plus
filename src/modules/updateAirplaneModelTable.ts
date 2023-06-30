e32fa46b4476ef61d682f93dfb6d707c62f89592
DTention <20673797+DTention@users.noreply.github.com>
2023-06-30 15:47:09 -0500

    Commented on updateAirplaneModelTable.ts
    Added comments using ChatGPT and reviewed responses for accuracy.

--- src/modules/updateAirplaneModelTable.ts
+++ src/modules/updateAirplaneModelTable.ts
@@ -1,33 +1,41 @@
+// Import necessary functions and modules
 import { calcFlightTime, calcFuelBurn, createLoadingElement, formatCurrency } from "~/helpers/utils"
 
+// Function to calculate the costs related to an airplane
 const calculateCosts = (plane: Plane, flightDuration: number, plane_category: number, utilisation: number) => {
-  const depreciationPercent = (100 / (plane.lifespan * 3)) * (1 + 2 * utilisation)
-  const depreciationRate = Math.floor(plane.price * (depreciationPercent / 100) * utilisation)
-  const maintenance = plane.capacity * 100 * utilisation
-  const airport_fee = (500 * plane_category + plane.capacity * 10) * 2
-  const crew_cost_per_hour = plane.capacity * (flightDuration / 60) * 12
-  const inflight_cost_per_passenger = (20 + (8 * flightDuration) / 60) * plane.capacity * 2
-
+  const depreciationPercent = (100 / (plane.lifespan * 3)) * (1 + 2 * utilisation) // Calculate the depreciation rate
+  const depreciationRate = Math.floor(plane.price * (depreciationPercent / 100) * utilisation) // Calculate the depreciation rate
+  const maintenance = plane.capacity * 100 * utilisation // Calculate the maintenance costs
+  const airport_fee = (500 * plane_category + plane.capacity * 10) * 2 // Calculate the airport fees
+  const crew_cost_per_hour = plane.capacity * (flightDuration / 60) * 12 // Calculate the crew cost per hour
+  const inflight_cost_per_passenger = (20 + (8 * flightDuration) / 60) * plane.capacity * 2 // Calculate the in-flight costs per passenger
+  
+  // Return the calculated costs
   return { depreciationRate, maintenance, airport_fee, crew_cost_per_hour, inflight_cost_per_passenger }
 }
 
+// Function to calculate the utilisation of an airplane
 const calculateUtilisation = (flightDuration: number, turnaroundTime: number) => {
-  const MAX_FLIGHT_MINUTES = 4 * 24 * 60
-  const totalTripTime = (flightDuration + turnaroundTime) * 2
-  const frequency = Math.floor(MAX_FLIGHT_MINUTES / totalTripTime)
-  const flightTime = frequency * totalTripTime
-  const availableFlightMinutes = MAX_FLIGHT_MINUTES - flightTime
-  const utilisation = flightTime / MAX_FLIGHT_MINUTES
-  const planeUtilisation = (MAX_FLIGHT_MINUTES - availableFlightMinutes) / MAX_FLIGHT_MINUTES
+  const MAX_FLIGHT_MINUTES = 4 * 24 * 60 // Define the maximum number of flight minutes
+  const totalTripTime = (flightDuration + turnaroundTime) * 2 // Calculate the total trip time
+  const frequency = Math.floor(MAX_FLIGHT_MINUTES / totalTripTime) // Calculate the frequency of flights
+  const flightTime = frequency * totalTripTime // Calculate the flight time
+  const availableFlightMinutes = MAX_FLIGHT_MINUTES - flightTime // Calculate the available flight minutes
+  const utilisation = flightTime / MAX_FLIGHT_MINUTES // Calculate the utilisation
+  const planeUtilisation = (MAX_FLIGHT_MINUTES - availableFlightMinutes) / MAX_FLIGHT_MINUTES // Calculate the plane utilisation
 
+  // Return the calculated utilisation values
   return { utilisation, planeUtilisation, frequency }
 }
 
+// Function to create cell contents
 function createCellContents(modelOwnerInfo: Plane): string[] {
+  // Check if the model is a favorite and create the cell content accordingly
   let cellContent = modelOwnerInfo.isFavorite
     ? `${modelOwnerInfo.name}<img src='assets/images/icons/heart.png' height='10px'>`
     : modelOwnerInfo.name
 
+  // Return the created cell contents
   return [
     cellContent,
     modelOwnerInfo.family,
@@ -45,26 +53,28 @@ function createCellContents(modelOwnerInfo: Plane): string[] {
   ]
 }
 
+// Function to create a row element
 function createRowElement(modelOwnerInfo: Plane, isOwned: boolean) {
-  const row = document.createElement("div")
+  const row = document.createElement("div") // Create the row
   row.className = "table-row clickable"
   row.setAttribute("data-model-id", modelOwnerInfo.id.toString())
-  row.style.background = isOwned ? "#465b65" : ""
-  row.onclick = () => window.selectAirplaneModel(window.loadedModelsById[modelOwnerInfo.id])
+  row.style.background = isOwned ? "#465b65" : "" // Set the row background if the model is owned
+  row.onclick = () => window.selectAirplaneModel(window.loadedModelsById[modelOwnerInfo.id]) // Set the onclick event handler for the row
   return row
 }
 
-// This function checks if any airplanes are assigned, available, or constructing.
+// Check if the model is owned by the user -- Checks if airplanes are assigned, available, or constructing.
 export const isModelOwned = (modelOwnerInfo: Plane) =>
   modelOwnerInfo.assignedAirplanes.length +
     modelOwnerInfo.availableAirplanes.length +
     modelOwnerInfo.constructingAirplanes.length !==
   0
 
-// This function checks if a plane is valid based on CPP, max capacity, and in-use value.
+// Check if the model is valid -- Checks if a plane is valid based on CPP, max capacity, and in-use value.
 export const isValidModel = (modelOwnerInfo: Plane, min_capacity: number, min_circulation: number) =>
   modelOwnerInfo.cpp !== -1 && modelOwnerInfo.max_capacity >= min_capacity && modelOwnerInfo.in_use >= min_circulation
 
+// Function to populate table cells
 const populateTableCells = (row: HTMLElement, cells: string[], title: string) => {
   cells.forEach((content, index) => {
     const cell = document.createElement("div")
@@ -77,16 +87,17 @@ const populateTableCells = (row: HTMLElement, cells: string[], title: string) =>
   })
 }
 
-/**
- * Creates a comparison function for sorting objects based on a given property.
- * The comparison function can be used directly in Array.prototype.sort().
- * If the values of the property in the objects are arrays, it sorts based on their lengths.
- * If the `ascending` parameter is false, it sorts in descending order, otherwise it sorts in ascending order.
- *
- * @param {string} property - The name of the property to sort the objects by.
- * @param {boolean} [ascending=true] - Indicates whether the sort order should be ascending (default) or descending.
- * @returns {(a: SortableObject, b: SortableObject) => number} - A comparison function that can be used in Array.prototype.sort().
- */
+// Function to create a comparison function for sorting 
+// --
+// Creates a comparison function for sorting objects based on a given property.
+// The comparison function can be used directly in Array.prototype.sort().
+// If the values of the property in the objects are arrays, it sorts based on their lengths.
+// If the `ascending` parameter is false, it sorts in descending order, otherwise it sorts in ascending order.
+
+// @param {string} property - The name of the property to sort the objects by.
+// @param {boolean} [ascending=true] - Indicates whether the sort order should be ascending (default) or descending.
+// @returns {(a: SortableObject, b: SortableObject) => number} - A comparison function that can be used in Array.prototype.sort().
+
 function sortByProperty(property: string, ascending: boolean = true): (a: SortableObject, b: SortableObject) => number {
   const sortOrder = ascending ? 1 : -1
 
@@ -102,7 +113,9 @@ function sortByProperty(property: string, ascending: boolean = true): (a: Sortab
   }
 }
 
+// Async function to update the airplane model table
 export async function updateAirplaneModelTable(sortProperty: string, sortOrder: string): Promise<void> {
+  // Get all necessary user input values
   const distance = Number((document.getElementById("fightRange") as HTMLInputElement).value)
   const runway = Number((document.getElementById("runway") as HTMLInputElement).value)
   const min_capacity = Number((document.getElementById("min_capacity") as HTMLInputElement).value)
@@ -110,6 +123,7 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
   const use_flight_total = (document.getElementById("fuel_total") as HTMLInputElement).checked
   const airplaneModelTable = document.getElementById("airplaneModelTable")
 
+  // Define the airplane type map
   const airplaneTypeMap = {
     LIGHT: 1,
     SMALL: 1,
@@ -121,8 +135,10 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
     JUMBO: 18,
     SUPERSONIC: 12
   }
+  
   let plane: Plane
   for (plane of Object.values(window.loadedModelsOwnerInfo)) {
+    // Check if the range is enough and the runway requirement is not too high
     if (plane.range < distance || plane.runwayRequirement > runway) {
       plane.cpp = -1
       plane.max_rotation = -1
@@ -132,10 +148,13 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
     const plane_category = airplaneTypeMap[plane.airplaneType.toUpperCase()]
     const flightDuration = calcFlightTime(plane, distance)
 
+    // Calculate the utilization and frequency
     const { utilisation, frequency } = calculateUtilisation(flightDuration, plane.turnaroundTime)
+    // Calculate the costs
     const { depreciationRate, maintenance, airport_fee, crew_cost_per_hour, inflight_cost_per_passenger } =
       calculateCosts(plane, flightDuration, plane_category, utilisation)
 
+    // Update the plane information
     plane.max_rotation = frequency
     plane.fbpf = calcFuelBurn(plane, distance)
     plane.fbpp = plane.fbpf / plane.capacity
@@ -152,6 +171,7 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
       " / Per Flight: $" +
       formatCurrency(Math.round(plane.cpp * plane.capacity))
 
+    // Check if the plane is in use
     if (plane.in_use === undefined) {
       plane.in_use = -1
 
@@ -167,6 +187,7 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
     }
   }
 
+  // If there is no sort property and sort order, get the default ones
   if (!sortProperty && !sortOrder) {
     const selectedSortHeader = document.querySelector("#airplaneModelSortHeader .cell.selected") as HTMLElement
     sortProperty = selectedSortHeader.getAttribute("data-sort-property")
@@ -177,30 +198,35 @@ export async function updateAirplaneModelTable(sortProperty: string, sortOrder: 
     }
     sortOrder = selectedSortHeader.getAttribute("data-sort-order")
   }
-  //sort the list
+  // Sort the list
   window.loadedModelsOwnerInfo.sort(sortByProperty(sortProperty, sortOrder == "ascending"))
 
+  // Clear the existing rows
   var childElements = airplaneModelTable.querySelectorAll("div.table-row")
-
+  
   childElements.forEach(function (child) {
     airplaneModelTable.removeChild(child)
   })
 
   let modelOwnerInfo: Plane
   for (modelOwnerInfo of Object.values(window.loadedModelsOwnerInfo)) {
+    // If the model is not in use, set it to 0
     modelOwnerInfo.in_use = modelOwnerInfo.in_use || 0
 
     const isOwned = isModelOwned(modelOwnerInfo)
 
+    // If the model is not valid, skip it
     if (!isValidModel(modelOwnerInfo, min_capacity, min_circulation)) {
       continue
     }
 
+    // Create a row element and populate its cells
     const row = createRowElement(modelOwnerInfo, isOwned)
     const cells = createCellContents(modelOwnerInfo)
 
     populateTableCells(row, cells, modelOwnerInfo.fuel_total_info)
 
+    // Append the row to the table
     airplaneModelTable.appendChild(row)
   }
 }
